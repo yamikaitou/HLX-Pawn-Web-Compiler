fa189677b5d409ab6390e2f446fa6d89c5131f1c
Ryan LeBlanc <yamikaitou@gmail.com>
2011-06-16 21:23:57 -0500

    Added defaults for the version tables

--- SQLiteSetup.php
+++ SQLiteSetup.php
@@ -6,7 +6,9 @@ if (!file_exists("configs"))
 $sql = sqlite_open("configs/data");
 
 sqlite_exec($sql, "CREATE TABLE amxxversions (ID INTEGER PRIMARY KEY, Name TEXT, Folder TEXT)");
+sqlite_exec($sql, "INSERT INTO amxxversions VALUES(NULL, '', '');");
 sqlite_exec($sql, "CREATE TABLE smversions (ID INTEGER PRIMARY KEY, Name TEXT, Folder TEXT)");
+sqlite_exec($sql, "INSERT INTO smversions VALUES(NULL, '', '');");
 sqlite_exec($sql, "CREATE TABLE info (ID INTEGER PRIMARY KEY, Name TEXT, Value TEXT);");
 sqlite_exec($sql, "INSERT INTO info VALUES(1, 'Key', 'NHzwGmAggt3e');");
 sqlite_exec($sql, "INSERT INTO info VALUES(2, 'AMXXComp', '.');");
