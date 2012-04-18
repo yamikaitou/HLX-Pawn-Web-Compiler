495564cb43e49867858e0567bb709814d9de0a79
Yami Kaitou <yamikaitou@gmail.com>
2012-04-18 18:16:31 -0500

    another typo

--- include/functions.php
+++ include/functions.php
@@ -5,7 +5,7 @@ function _init($install=FALSE)
 	$db_type = NULL;
 	$general = array('key'=>NULL, 'amxxcomp'=>NULL, 'smcomp'=>NULL, 'compiled'=>NULL, 'temp'=>NULL, 'vbupload'=>NULL, 'ipbupload'=>NULL);
 	$sqlite = array('db'=>NULL);
-	$mysql = array('server'=>NULL, 'user',=>NULL, 'pass'=>NULL, 'db'=>NULL, 'prefix'=>NULL);
+	$mysql = array('server'=>NULL, 'user'=>NULL, 'pass'=>NULL, 'db'=>NULL, 'prefix'=>NULL);
 	$dynamodb = array('access'=>NULL, 'secret'=>NULL, 'zone'=>NULL, 'prefix'=>NULL, 'suffix'=>NULL);
 	
 	require_once("config.php");
