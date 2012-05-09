08f2a55280a25afea29f0aa819ade94823c878f2
Yami Kaitou <yamikaitou@gmail.com>
2012-05-09 00:23:03 -0500

    Project Renamed

--- include/template.php
+++ include/template.php
@@ -5,14 +5,14 @@ function style_top($title)
 ?>
 <html>
 <head>
-<title>SuperCentral - Compiler</title>
+<title>HLx Pawn Web Compiler</title>
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="bluebliss.css" />
 </head>
 <body>
 <div id="mainContentArea">
 <div id="contentBox">
-<div id="title">SuperCentral Compiler</div>
+<div id="title"></div>
 
 <div id="linkGroup">
 <div class="link"><a href="index.html">Home</a></div>
@@ -34,7 +34,7 @@ function style_bot()
 ?>
 </div>
 <br/>
-<div id="footer">design by <a href="http://www.bryantsmith.com">bryant smith</a> | script by <a href="https://github.com/yamikaitou/Supercentral-Compiler">ryan leblanc</a> </div>
+<div id="footer">design by <a href="http://www.bryantsmith.com">bryant smith</a> | script by <a href="https://github.com/yamikaitou/HLX-Pawn-Web-Compiler">ryan leblanc</a> </div>
 </div>
 </div>
 </div>
